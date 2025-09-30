a620e6743dc93bf5044a98f08842a8d555085721
OrehkinaVita <oreshkinavitalia@yandex.ru>
2025-09-30 21:49:56 +0300

    Add files via upload

---
+++ vita№1.cpp
@@ -0,0 +1,30 @@
+﻿// vita№1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <math.h>
+double myE(double x, double eps)
+{
+	double cn = 1;
+	double s = cn;
+	for (int n = 1; fabs(cn) > eps; n++)
+	{
+		cn = -cn * x / n;
+		s += cn;
+	}
+	return s;
+}
+
+int main()
+{
+	double xstart = 0.0;
+	double xend = 2.0;
+	double dx = 0.2;
+	double eps = 0.0001;
+	for (double x = xstart; x <= xend + 1e-10; x += dx)
+	{
+		double s = myE(x, eps);
+		printf("%f %f\n", x, s);
+	}
+	return 0;
+}
